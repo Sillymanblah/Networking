5a0ea4db2aa9b5ef6a40ddd4a80f78ee568a8aa9
John Grzegorczyk <jtgrzeg@gmail.com>
2025-05-12 14:43:12 -0500

    Added the new check for waiting connections (with notes), and a try block for the accept call, so the server does not crash.

--- src/drivers/server.cpp
+++ src/drivers/server.cpp
@@ -50,10 +50,29 @@ int main()
 
 		server.listen( maximum_connections );
 
-		while ( !shutdown )
+		do
 		{
-			connection_handlers.emplace_back( handle_connection, server.accept() );
+			std::clog << "Checking for connections...\n";
+
+			// Note that `connection_waiting` may or may not be a blocking call depending on the system, though from testing it appears to block until there is a connection waiting, so it might be more apt to call it `await_connection`.
+			if ( server.connection_waiting() && !shutdown )
+			{
+				try
+				{
+					stream_socket new_connection( server.accept() );
+					std::clog << "Accepted the connection!\nPassing it on to the handler...\n";
+					connection_handlers.emplace_back( handle_connection, std::move( new_connection ) );
+				}
+				catch ( const socket_error& err ) {} // Do nothing the only error this should be is a failed accept operation that we can ignore.
+			}
+			// In the event that `connection_waiting` is non-blocking on the socket for some reason, which would mean someone used a hacky workaround to implant their own `socket_ptr` into `server_socket`, there is a catch here.
+			else
+			{
+				std::this_thread::sleep_for( std::chrono::seconds( 1 ) ); // Server waits a moment so we don't burn a million resources constantly checking if there is a connection.
+				std::clog << "There were no connections to accept...\n";
+			}
 		}
+		while ( !shutdown );
 	}
 	catch ( const std::runtime_error& error ) { std::cerr << error.what(); }
 
