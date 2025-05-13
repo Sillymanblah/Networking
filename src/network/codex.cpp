c3fbad9b742ef1ad225b6a228ea31b293f21d5e7
John Grzegorczyk <jtgrzeg@gmail.com>
2025-05-12 22:40:34 -0500

    Basic examples of how codex can work.

--- src/network/codex.cpp
+++ src/network/codex.cpp
@@ -18,5 +18,43 @@ namespace network
 
 	void codex::decrypt( const char* input, char* output )
 	{ do_decrypt( input, output ); }
+
+	basic_codex::basic_codex( char offset ) : offset( offset % max_offset ) {} // Restrict offset to a byte
+
+	char basic_codex::do_shift( const char& input, char shift )
+	{ return input << shift | input >> ( max_offset - shift ); }
+
+	void basic_codex::do_encrypt( const char* input, char* output )
+	{
+		for ( size_t index = 0; index < std::strlen( input ); ++index ) 
+			output[ index ] = do_shift( input[ index ], this->offset );
+	}
+
+	void basic_codex::do_decrypt( const char* input, char* output )
+	{
+		for ( size_t index = 0; index < std::strlen( input ); ++index ) 
+			output[ index ] = do_shift( input[ index ], ( max_offset - this->offset % 8 ) );
+	}
+
+	recursion_codex::recursion_codex( char initial_modifier ) : modifier( initial_modifier ) {}
+
+	char recursion_codex::update_modifier( char next_modifier )
+	{
+		char prev_modifier = this->modifier;
+		this->modifier = next_modifier;
+		return prev_modifier;
+	}
+
+	void recursion_codex::do_encrypt( const char* input, char* output )
+	{
+		for ( size_t index = 0; index < std::strlen( input ); ++index )
+			this->update_modifier( output[ index ] = input[ index ] ^ this->modifier );
+	}
+
+	void recursion_codex::do_decrypt( const char* input, char* output )
+	{
+		for ( size_t index = 0; index < std::strlen( input ); ++index )
+			output[ index ] = input[ index ] ^ this->update_modifier( input[ index ] );
+	}
 }
 // namespace network
--- src/network/codex.hpp
+++ src/network/codex.hpp
@@ -28,6 +28,47 @@ namespace network
 		// Public decryption function that just calls the protected `do_decrypt` function, which should be overridden by a derived class.
 		void decrypt( const char* input, char* output );
 	};
+
+	// The below are some examples of how to use the codex, note that when building a codex, it is good practice to not have the codex publicly published for security reasons.
+
+	// Basic codex simply uses a bitwise algorithm
+	class basic_codex : public codex
+	{
+	public:
+		basic_codex( char offset );
+		static constexpr char max_offset = 8;
+
+	private:
+		static char do_shift( const char& input, char offset );
+
+	protected:
+		void do_encrypt( const char* input, char* output ) override;
+		void do_decrypt( const char* input, char* output ) override;
+	
+	private:
+		char offset;
+	};
+
+	// Recursion codex uses the previous set of data to xor the next set of data, which allows the server and client to vary throughout the communication cycle.
+	class recursion_codex : public codex
+	{
+	public:
+		// Constructor which can set the initial modifier value.
+		recursion_codex( char initial_modifier = 0 );
+
+	private:
+		// Updates the modifier and returns the previous one.
+		char update_modifier( char next_modifier );
+
+	protected:
+		// Uses modifier to update a character, then sets the modifier based on the result of that operation.
+		void do_encrypt( const char* input, char* output ) override;
+		// Uses modifier to update a character and then sets the modifier to the previously modified character.
+		void do_decrypt( const char* input, char* output ) override;
+
+	private:
+		char modifier;
+	};
 }
 // namespace network
 
